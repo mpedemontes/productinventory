1abe491a45e64615a193ae71715d4a70a0d95aaa
mpedemontesalvador <marc.pedemonte@cabsa.es>
2025-05-23 19:57:52 +0200

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/phoenix/productinventory/ProductinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix.productinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
