fa3759b48eb401d2f0c8afec370fec6b8d4e6e8b
mpedemontesalvador <marc.pedemonte@cabsa.es>
2025-05-26 17:49:45 +0200

    chore: enable Specification support in ProductRepository

--- src/main/java/com/phoenix/productinventory/repository/ProductRepository.java
+++ src/main/java/com/phoenix/productinventory/repository/ProductRepository.java
@@ -2,8 +2,10 @@ package com.phoenix.productinventory.repository;
 
 import com.phoenix.productinventory.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 /** Repository interface for Product entity persistence. */
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+public interface ProductRepository
+    extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {}
